18c9ff5a3140c5cbd990752341a1517a6c433fb9
Muhammad Saad <saadmughal532@gmail.com>
2025-11-16 18:34:22 +0500

    Update tooltip text for FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Incremented by Pressing the Plus(+) Button',
         child: const Icon(Icons.add),
       ),
     );
